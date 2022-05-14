dd8c2fdada273db06ae34fbb0f79ac4929a901e8
Lucas Vieira <lucas.vieira.093@ufrn.edu.br>
2022-05-14 20:48:10 -0300

    first commit

---
+++ src/main/java/rn/sead/gov/WebfolhaApplication.java
@@ -0,0 +1,13 @@
+package rn.sead.gov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfolhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfolhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rn/sead/gov/WebfolhaApplicationTests.java
@@ -0,0 +1,13 @@
+package rn.sead.gov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfolhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
