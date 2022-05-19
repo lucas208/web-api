b250bb8a41ac6b336485867347b1a32f5dba0ed2
Lucas Vieira <lucas.vieira.093@ufrn.edu.br>
2022-05-19 16:32:33 -0300

    atualizando banco para update e name para nome

--- src/main/java/rn/sead/gov/model/Pessoa.java
+++ src/main/java/rn/sead/gov/model/Pessoa.java
@@ -23,7 +23,7 @@ public abstract class Pessoa extends AbstractEntity {
 
 	private static final long serialVersionUID = 1L;
 
-	private String name;
+	private String nome;
 
     private String email;
 
